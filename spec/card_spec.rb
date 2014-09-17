95f136826ae4f8fb834cc4201e3bee046ea9e7c1
Andrew Sinner <andrewsinner@gmail.com>
2014-09-17 09:29:31 -0700

    Appease the cop

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -6,12 +6,12 @@ describe Card do
     card = Card.new(suit: 'Spade')
     assert_equal(card.suit, 'Spade')
   end
-  
+
   it 'has a rank' do
     card = Card.new(rank: 'Jack')
     assert_equal(card.rank, 'Jack')
   end
-  
+
   it 'has a rank and suit' do
     card = Card.new(suit: 'Heart', rank: 'Ace')
     assert_equal(card.suit, 'Heart')
